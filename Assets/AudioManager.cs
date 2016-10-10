b381be68057ef10f655d55f2267d0aef5c23daf5
lbrand-poyo <happydriven@gmail.com>
2016-10-10 20:08:17 +0900

    リミッター搭載

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -4,16 +4,25 @@ using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour {
 
-	AudioSource SE;
+	AudioSource SE1;
+	AudioSource SE2;
+	int PlayCountCheck;
 	AudioClip[] SE_Clips = new AudioClip[5];
-	public const int SE_NPOYO = 1;
-	public const int SE_NZZ = 2;
+	public const int SE_POYO1 = 0;
+	public const int SE_POYO2 = 1;
+	public const int SE_POYOYO = 2;
+	public const int SE_NPOYO = 3;
+	public const int SE_NZZ = 4;
 
 	void Start () {
-		SE = GameObject.Find ("GameManager/AudioManager/SE").GetComponent<AudioSource> ();
+		PlayCountCheck = 0;
+		SE1 = GameObject.Find ("GameManager/AudioManager/SE").GetComponent<AudioSource> ();
+		SE_Clips [SE_POYO1] = Resources.Load<AudioClip> ("Audio/SE/Poyo1_poyo");
+		SE_Clips [SE_POYO2] = Resources.Load<AudioClip> ("Audio/SE/Poyo2_poyo~");
+		SE_Clips [SE_POYOYO] = Resources.Load<AudioClip> ("Audio/SE/Poyo4_poyoyo");
 		SE_Clips [SE_NPOYO] = Resources.Load<AudioClip> ("Audio/SE/Poyo3_npoyo");
 		SE_Clips [SE_NZZ] = Resources.Load<AudioClip> ("Audio/SE/nzz1");
-		
+	
 	}
 
 	
@@ -23,13 +32,18 @@ public class AudioManager : MonoBehaviour {
 
 
 	public void PlaySE(int index){			
-		SE.clip = SE_Clips[index];
-		SE.Play();
-
+		if (PlayCountCheck < 4) {
+			SE1.clip = SE_Clips [index];
+			SE1.PlayOneShot (SE1.clip);
+			PlayCountCheck = PlayCountCheck + 1;
+		}else{
+			PlayCountCheck = PlayCountCheck -2;
+		}
 	}
 
 
 }
+
 
 
 
